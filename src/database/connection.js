0d10e6fbbd8c65bdb5de5d73e3107eea2a1e0770
PedroPaduelo <pedropaduelo@gmail.com>
2021-03-01 03:22:43 +0000

    first

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,10 +3,11 @@ const knex = require('knex');
 const connection = knex({
   client: 'pg',
   connection: {
-      host : 'localhost',
-      database: 'gestaoprods',
-      user:     'postgres',
-      password: '4063020'
+        host : "postgresql-21633-0.cloudclusters.net",
+        port: 21633,
+        user : "Nommand",
+        password : "406302077",
+        database : "gestaoprods",
   },
   useNullAsDefault: true
 })
