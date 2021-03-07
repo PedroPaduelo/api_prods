6130a7ad8082b5cee5207fb218d54de191b064da
PedroPaduelo <pedropaduelo@gmail.com>
2021-03-07 23:05:41 +0000

    correção BD

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,7 +3,7 @@ const knex = require('knex');
 const connection = knex({
   client: 'pg',
   connection: {
-        host : "Host : postgresql-21633-0.cloudclusters.net",
+        host : "postgresql-21633-0.cloudclusters.net",
         port: 21633,
         user : "postgres",
         password : "4063020",
